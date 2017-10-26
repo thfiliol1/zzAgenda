0216353b64d138a6551a1d7a39cb258be48ccb64
stef-linux <stef-linux@linux>
2017-10-26 14:58:09 +0200

    Ajout des langues

---
+++ lang/en.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$language['schedules']='Schedules';
+$language['map']='Map';
+$language['logout']='Logout';
+$language['login']='Login';
+$language['password']='Password';
+$language['login']='Login';
+$language['administration']='Administration';
+$language['date']='Date';
+$language['title']='Title';
+$language['description']='Description';
+$language['city']='City';
+$language['speaker']='Speaker';
+$language['edit']='Edit';
+$language['delete']='Delete';
+$language['add_schedule']='Add schedule';
+$language['add']='Add';
+$language['update']='Update';
+$language['delete_entry']='Delete this entry';
+$language['delete_question']='Are you sure you want to delete this Record';
+$language['yes']='Yes';
+$language['no']='No';
---
+++ lang/fr.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$language['schedules']='Conférences';
+$language['map']='Carte';
+$language['logout']='Déconnexion';
+$language['login']='Connexion';
+$language['password']='Mot de passe';
+$language['login']='Se connecter';
+$language['administration']='Administration';
+$language['date']='Date';
+$language['title']='Titre';
+$language['description']='Description';
+$language['city']='Ville';
+$language['speaker']='Orateur';
+$language['edit']='Editer';
+$language['delete']='Supprimer';
+$language['add_schedule']='Ajouter une conférence';
+$language['add']='Ajouter';
+$language['update']='Mettre à jour';
+$language['delete_entry']='Supprimer une entrée';
+$language['delete_question']='Êtes-vous sûr de vouloir supprimer cet enregistrement';
+$language['yes']='Oui';
+$language['no']='Non';
